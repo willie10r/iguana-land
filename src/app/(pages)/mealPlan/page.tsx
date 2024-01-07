2322ecff3af665eb863965b7de10067fedbcf889
willie m <zx10rwillie@gmail.com>
2024-01-06 22:07:22 -0600

    Fixed the next.js config file and got the menu function working.
    Got the menu function working just want to go back and fic the redundant code and get the reshuffle button working. I also have to go back and get the 1440p stylinging fixed

--- src/app/(pages)/mealPlan/page.tsx
+++ src/app/(pages)/mealPlan/page.tsx
@@ -8,7 +8,7 @@ import { useEffect, useState } from 'react';
 import supabase from '../../supabase';
 
 export default function MealPlan() {
-  const [data, setData] = useState<{ id: number; name: string; staple: boolean }[]>([]);
+  const [data, setData] = useState<{ id: number; name: string; staple: boolean; img_id: string }[]>([]);
     useEffect(() => {
         const fetchData = async () => {
         try {
@@ -29,21 +29,19 @@ export default function MealPlan() {
         fetchData();
     }, []);
 // section for the random 4 part menu
-    let randomFood =data[randomNumber()];
+    let randomFood =data[0];
     //errorhandling for the returned data
     if (!randomFood) {
         console.error('Error: randomFood is undefined');
         // Handle the error or return from the function
         return;
         }
+
+//I would like to refactor this part of the code to run as a loop so I can get rid of the redundent parts
     let foodNameOne = randomFood;
-    let stapleOne = randomFood; 
     let foodNameTwo = randomFood;
-    let stapleTwo = randomFood;
     let foodNameThree = randomFood;
-    let stapleThree = randomFood;
     let foodNameFour = randomFood;
-    let stapleFour = randomFood;
     
     function randomNumber():number {
         return Math.floor(Math.random() * data.length);
@@ -52,45 +50,33 @@ export default function MealPlan() {
     function MenuOne()  {
         let foodSet = data[randomNumber()]; 
         foodNameOne = foodSet;
-        stapleOne = foodSet;
-
-
-        return console.log('working');
+      
         
     };
     function MenuTwo()  {
         let foodSet = data[randomNumber()]; 
         foodNameTwo = foodSet;
-        stapleTwo = foodSet;
-
-
-        return console.log('working');
         
     };
     function MenuThree()  {
         let foodSet = data[randomNumber()]; 
         foodNameThree = foodSet;
-        stapleThree = foodSet;
-
-
-        return console.log('working');
         
     };
     function MenuFour()  {
         let foodSet = data[randomNumber()]; 
         foodNameFour = foodSet;
-        stapleFour = foodSet;
-
-
-        return console.log('working');
         
     };
     MenuOne();
     MenuTwo();
     MenuThree();
     MenuFour();
+
+    //This section will be the resuffle part
+
+
 // for loging and testing
-    console.log(randomFood);
 
 
     return (
@@ -115,44 +101,52 @@ export default function MealPlan() {
                 <h2 className='text-center mb-32 font-bold text-3xl'>Weekly Meal</h2>
                 <div className='text-center'>
                     <ul className=' flex flex-col xl:grid xl:grid-cols-2 xl:gap-6 mb-24'>
-                        <li className='mb-5 bg-green-500 w-60 h-72 rounded-3xl mx-auto'>
-                            <Image className=' mb-6'
-                                src=""
+                        <li className=' mb-5 bg-green-500 w-60 h-72 rounded-3xl mx-auto'>
+                            <Image className='  mx-auto  mb-2'
+                                src={`${foodNameOne.img_id}`}
                                 alt="pic of food"
+                                width={100}
+                                height={50}
                             />
                             <h3 className=' mb-5 font-semibold text-2xl'>{ `${foodNameOne.name}`}</h3>
                             <p className=' mb-2 font-semibold text-lg'>Is this food a staple? </p>
-                            <p className=' mb-5 font-semibold text-lg'> { `${stapleOne.staple}`}</p>
+                            <p className=' mb-5 font-semibold text-lg'> { `${foodNameOne.staple}`}</p>
                             <p>info on food </p>
                         </li>
                         <li className='mb-5 bg-green-500 w-60 h-72 rounded-3xl mx-auto'>
-                            <Image className=' mb-6'
-                                src=""
-                                alt="pic of food"
+                            <Image className='mx-auto mb-6'
+                                    src={`${foodNameTwo.img_id}`}
+                                    alt="pic of food"
+                                    width={100}
+                                    height={50}
                             />
                             <h3 className=' mb-5 font-semibold text-2xl'>{ `${foodNameTwo.name}`}</h3>
                             <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                            <p className=' mb-5 font-semibold text-lg'> { `${stapleTwo.staple}`}</p>
+                            <p className=' mb-5 font-semibold text-lg'> { `${foodNameTwo.staple}`}</p>
                             <p>info on food</p>
                         </li>
                         <li className='mb-5 bg-green-500 w-60 h-72 rounded-3xl mx-auto'>
-                            <Image className=' mb-6'
-                                src=""
-                                alt="pic of food"
+                            <Image className='mx-auto mb-6'
+                                   src={`${foodNameThree.img_id}`}
+                                   alt="pic of food"
+                                   width={100}
+                                   height={50}
                             />
                             <h3 className=' mb-5 font-semibold text-2xl'>{ `${foodNameThree.name}`}</h3>
                             <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                            <p className=' mb-5 font-semibold text-lg'> { `${stapleThree.staple}`}</p>
+                            <p className=' mb-5 font-semibold text-lg'> { `${foodNameThree.staple}`}</p>
                             <p>info on food</p>
                         </li>
                         <li className='mb-5 bg-green-500 w-60 h-72 rounded-3xl mx-auto'>
-                            <Image className=' mb-6'
-                                src=""
-                                alt="pic of food"
+                            <Image className='mx-auto mb-6'
+                                    src={`${foodNameFour.img_id}`}
+                                    alt="pic of food"
+                                    width={100}
+                                    height={50}
                             />
                             <h3 className=' mb-5 font-semibold text-2xl'>{ `${foodNameFour.name}`}</h3>
                             <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                            <p className=' mb-5 font-semibold text-lg'> { `${stapleFour.staple}`}</p>
+                            <p className=' mb-5 font-semibold text-lg'> { `${foodNameFour.staple}`}</p>
                             <p>info on food</p>
                         </li>
                     </ul>
