c836d435bd0c086331b7e942333e09ff2d274dc0
willie10r <zx10rwillie@gmail.com>
2024-01-10 14:18:28 -0600

    Added two more table's in the database so I had to rework the code. I would like to refactor it after I get the site up on the internet.

--- src/app/(pages)/mealPlan/page.tsx
+++ src/app/(pages)/mealPlan/page.tsx
@@ -10,17 +10,33 @@ import { useEffect, useState } from 'react';
 import supabase from '../../thebase';
 
 export default function MealPlan() {
-    let [foodData, setFoodData] = useState<{ id: number; name: string; staple: boolean; img_id: string; nutrition: string }[]>([]);
-    let [treatData, setTreatData] = useState<{ id: number; name: string; staple: boolean; img_id: string }[]>([]);
+    let [mainGreenData, setMainGreenData] = useState<{ id: number; name: string; staple: boolean; img_id: string; nutrition: string }[]>([]);
+    let [secGreenData, setSecGreenData] = useState<{ id: number; name: string; staple: boolean; img_id: string; nutrition: string }[]>([]);
+    let [topperData, setTopperData] = useState<{ id: number; name: string; staple: boolean; img_id: string; nutrition: string }[]>([]);
+    let [treatData, setTreatData] = useState<{ id: number; name: string; staple: boolean; img_id: string; nutrition: string }[]>([]);
 
     let fetchData = async () => {
         try {
-            const { data: foodData, error: foodError } = await supabase
-                .from('food')
+            const { data: mainGreenData, error: mainGreenError } = await supabase
+                .from('maingreens')
                 .select('*');
 
-            if (foodError) {
-                throw foodError;
+            if (mainGreenError) {
+                throw mainGreenError;
+            }
+            const { data: secGreenData, error: secGreenError } = await supabase
+                .from('secgreen')
+                .select('*');
+
+            if (secGreenError) {
+                throw secGreenError;
+            }
+            const { data: topperData, error: topperError } = await supabase
+                .from('toppers')
+                .select('*');
+
+            if (topperError) {
+                throw topperError;
             }
             const { data: treatData, error: treatError } = await supabase
                 .from('treats')
@@ -30,7 +46,9 @@ export default function MealPlan() {
                 throw treatError;
             }
 
-            setFoodData(foodData);
+            setMainGreenData(mainGreenData);
+            setSecGreenData(secGreenData);
+            setTopperData(topperData);
             setTreatData(treatData);
             console.log('load set');
         } catch (error) {
@@ -45,19 +63,26 @@ export default function MealPlan() {
     // section for the random 4 part menu
 
     //errorhandling for the returned data
-    if (!foodData[0]) {
+    if (!mainGreenData[0]) {
         console.error('Error: randomFood is undefined');
         // Handle the error or return from the function
         return;
     }
-    let menu = Array.from({ length: 3 }, () => getRandomFood());
-    let treatMenu = Array.from({ length: 1 }, () => getRandomTreat());
+    let [mainGreen] = Array.from({ length: 1 }, () => getRandomMainGreen());
+    let [secGreen] = Array.from({ length: 1 }, () => getRandomSecGreen());
+    let [topper] = Array.from({ length: 1 }, () => getRandomTopper());
+    let [treat] = Array.from({ length: 1 }, () => getRandomTreat());
 
-    let [foodNameOne, foodNameTwo, foodNameThree] = menu;
-    let [treatName] = treatMenu;
 
-    function randomFoodNumber(): number {
-        return Math.floor(Math.random() * foodData.length);
+
+    function randomGreenNumber(): number {
+        return Math.floor(Math.random() * mainGreenData.length);
+    };
+    function randomSecGreenNumber(): number {
+        return Math.floor(Math.random() * secGreenData.length);
+    };
+    function randomTopperNumber(): number {
+        return Math.floor(Math.random() * topperData.length);
     };
     function randomTreatNumber(): number {
         return Math.floor(Math.random() * treatData.length);
@@ -65,11 +90,17 @@ export default function MealPlan() {
 
 
 
-    function getRandomFood(): { id: number; name: string; staple: boolean; img_id: string; nutrition: string } {
-        return foodData[randomFoodNumber()];
+    function getRandomMainGreen(): { id: number; name: string; staple: boolean; img_id: string; nutrition: string } {
+        return mainGreenData[randomGreenNumber()];
+    }
+    function getRandomSecGreen(): { id: number; name: string; staple: boolean; img_id: string; nutrition: string } {
+        return secGreenData[randomSecGreenNumber()];
+    }
+    function getRandomTopper(): { id: number; name: string; staple: boolean; img_id: string; nutrition: string } {
+        return topperData[randomTopperNumber()];
     }
 
-    function getRandomTreat(): { id: number; name: string; staple: boolean; img_id: string } {
+    function getRandomTreat(): { id: number; name: string; staple: boolean; img_id: string; nutrition: string } {
         return treatData[randomTreatNumber()];
     }
 
@@ -103,58 +134,62 @@ export default function MealPlan() {
                 <div className='text-center'>
                     <ul className=' flex flex-col xl:grid xl:grid-cols-2 xl:gap-6 mb-24'>
                         <li className=' mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
+                            <h2 className=' mb-5 font-semibold text-3xl'>Main green</h2>
                             <Image className='  mx-auto  my-6 rounded-md'
-                                src={`${foodNameOne.img_id}`}
+                                src={`${mainGreen.img_id}`}
                                 alt="pic of food"
                                 width={100}
                                 height={50}
                             />
-                            <h3 className=' mb-5 font-semibold text-2xl'>{`${foodNameOne.name}`}</h3>
+                            <h3 className=' mb-5 font-semibold text-2xl'>{`${mainGreen.name}`}</h3>
                             <p className=' mb-2 font-semibold text-lg'>Is this food a staple? </p>
-                            <p className=' mb-5 font-semibold text-lg'> {`${foodNameOne.staple}`}</p>
-                            <p{`${foodNameOne.nutrition}`}}</p>
-                    </li>
-                    <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
-                        <Image className='mx-auto my-6 rounded-md'
-                            src={`${foodNameTwo.img_id}`}
-                            alt="pic of food"
-                            width={100}
-                            height={50}
-                        />
-                        <h3 className=' mb-5 font-semibold text-2xl'>{`${foodNameTwo.name}`}</h3>
-                        <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                        <p className=' mb-5 font-semibold text-lg'> {`${foodNameTwo.staple}`}</p>
-                        <p>info on food</p>
-                    </li>
-                    <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
-                        <Image className='mx-auto my-6 rounded-md'
-                            src={`${foodNameThree.img_id}`}
-                            alt="pic of food"
-                            width={100}
-                            height={50}
-                        />
-                        <h3 className=' mb-5 font-semibold text-2xl'>{`${foodNameThree.name}`}</h3>
-                        <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                        <p className=' mb-5 font-semibold text-lg'> {`${foodNameThree.staple}`}</p>
-                        <p>info on food</p>
-                    </li>
-                    <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
-                        <Image className='mx-auto my-6 rounded-md'
-                            src={`${treatName.img_id}`}
-                            alt="pic of food"
-                            width={100}
-                            height={50}
-                        />
-                        <h3 className=' mb-5 font-semibold text-2xl'>{`${treatName.name}`}</h3>
-                        <p className=' mb-5 font-semibold text-lg'>staple or not staple</p>
-                        <p className=' mb-5 font-semibold text-lg'> {`${treatName.staple}`}</p>
-                        <p>info on food</p>
-                    </li>
-                </ul>
-                <p className='text-center px-4 mb-12 text-2xl'>If you don&apos;t like what you see or the items are not in your location here&apos;s a resuffle button.</p>
-                <button onClick={reshuffle} className='text-center w-40 bg-green-500 inline-block border rounded-lg py-2 px-4 text-black hover:text-white font-semibold mb-60'>resuffle</button>
-            </div>
-        </section >
+                            <p className=' mb-5 font-semibold text-lg'> {`${mainGreen.staple}`}</p>
+                            <p>{`${mainGreen.nutrition}`}</p>
+                        </li>
+                        <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
+                            <h2 className=' mb-5 font-semibold text-3xl'>Secondary green</h2>
+                            <Image className='mx-auto my-6 rounded-md'
+                                src={`${secGreen.img_id}`}
+                                alt="pic of food"
+                                width={100}
+                                height={50}
+                            />
+                            <h3 className=' mb-5 font-semibold text-2xl'>{`${secGreen.name}`}</h3>
+                            <p className=' mb-2 font-semibold text-lg'>staple or not staple</p>
+                            <p className=' mb-5 font-semibold text-lg'> {`${secGreen.staple}`}</p>
+                            <p>{`${secGreen.nutrition}`}</p>
+                        </li>
+                        <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
+                            <h2 className=' mb-5 font-semibold text-3xl'>Topper</h2>
+                            <Image className='mx-auto my-6 rounded-md'
+                                src={`${topper.img_id}`}
+                                alt="pic of food"
+                                width={100}
+                                height={50}
+                            />
+                            <h3 className=' mb-5 font-semibold text-2xl'>{`${topper.name}`}</h3>
+                            <p className=' mb-2 font-semibold text-lg'>staple or not staple</p>
+                            <p className=' mb-5 font-semibold text-lg'> {`${topper.staple}`}</p>
+                            <p>{`${topper.nutrition}`}</p>
+                        </li>
+                        <li className='mb-5 bg-green-500 w-64 h-96 rounded-3xl mx-auto'>
+                            <h2 className=' mb-5 font-semibold text-3xl'>Treat</h2>
+                            <Image className='mx-auto my-6 rounded-md'
+                                src={`${treat.img_id}`}
+                                alt="pic of food"
+                                width={100}
+                                height={50}
+                            />
+                            <h3 className=' mb-5 font-semibold text-2xl'>{`${treat.name}`}</h3>
+                            <p className=' mb-2 font-semibold text-lg'>staple or not staple</p>
+                            <p className=' mb-5 font-semibold text-lg'> {`${treat.staple}`}</p>
+                            <p>{`${treat.nutrition}`}</p>
+                        </li>
+                    </ul>
+                    <p className='text-center px-4 mb-12 text-2xl'>If you don&apos;t like what you see or the items are not in your location here&apos;s a resuffle button.</p>
+                    <button onClick={reshuffle} className='text-center w-40 bg-green-500 inline-block border rounded-lg py-2 px-4 text-black hover:text-white font-semibold mb-60'>resuffle</button>
+                </div>
+            </section >
         </>
     )
 }
